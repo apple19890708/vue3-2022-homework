2dbdecb6d202c83b96fe1caf63727781fb2ebe54
Tommy <Tommychen@applyeasypro.com>
2022-01-21 10:50:00 +0800

    [feat]新增後台產品管理頁面與登入畫面

--- hexschool-homework-week-2/src/views/Home.vue
+++ hexschool-homework-week-2/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="card">
+      <div class="card-body">This is some text within a card body.</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
   },
